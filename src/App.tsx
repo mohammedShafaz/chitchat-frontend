5510b35fa3f8925f8e4aae991d5f762fd8de3ab4
mohammedShafaz <mohammedshafaz007@gmail.com>
2024-08-14 19:51:47 +0530

    client side basic setup

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import LandingPage from "./pages/LandingPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div>Welcome to chitchat folks!</div>
+  )
+}
+
+export default LandingPage
