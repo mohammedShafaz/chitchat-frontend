0e4fd7673adca76c28a8fc53e934b6d6b0a3951e
mohammedShafaz <mohammedshafaz007@gmail.com>
2024-12-23 19:03:25 +0530

    refactoring

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const url: string = import.meta.env.VITE_NODE_ENV === "development" ? 'http://localhost:5000/api/v1' : import.meta.env.VITE_BASE_PATH;
+
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BASE_PATH || 'http://localhost:5001/api/v1',
- 
+    baseURL: url,
+
 });
 
 export default axiosInstance;
